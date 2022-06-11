cbb189eaa252b2bb8b5cb659509900b309443da5
alikaratas17 <alikaratas17@ku.edu.tr>
2022-06-11 15:02:09 +0300

    Improved player code

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -7,28 +7,42 @@ public class movement : MonoBehaviour
     private Rigidbody myRigidbody;
     // Start is called before the first frame update
 int turning;
+bool going_forward;
+bool going_right;
+bool going_left;
+bool going_back;
+bool jumping;
     void Start()
     {
         turning = 0;
+        going_forward=false;
+        going_right=false;
+        going_left=false;
+        going_back=false;
+        jumping=false;
         myRigidbody = transform.GetComponent<Rigidbody>();
         
     }
-    // Update is called once per frame
-    void Update()
-    {
-        transform.eulerAngles =new Vector3(0, transform.eulerAngles.y,0);
+    void getKeyPresses(){
         if(Input.GetKeyDown(KeyCode.W)){
-            myRigidbody.AddForce(transform.forward*200f);
-            transform.Rotate(-Vector3.up*0.2f);
+            going_forward = true;
+        }else if(Input.GetKeyUp(KeyCode.W)){
+            going_forward = false;
         }
         if(Input.GetKeyDown(KeyCode.D)){
-            myRigidbody.AddForce(transform.right*200f);
+            going_right =true ;
+        }else if(Input.GetKeyUp(KeyCode.D)){
+            going_right = false;
         }
         if(Input.GetKeyDown(KeyCode.A)){
-            myRigidbody.AddForce(-transform.right*200f);
+            going_left =true ;
+        }else if(Input.GetKeyUp(KeyCode.A)){
+            going_left = false;
         }
         if(Input.GetKeyDown(KeyCode.S)){
-            myRigidbody.AddForce(-transform.forward*200f);
+            going_back =true ;
+        }else if(Input.GetKeyUp(KeyCode.S)){
+            going_back = false;
         }
         if(Input.GetKeyDown(KeyCode.RightArrow)){
             turning = 2;
@@ -43,13 +57,56 @@ int turning;
             turning = 0;
         }
         if(Input.GetKeyDown(KeyCode.Space)){
-            myRigidbody.AddForce(Vector3.up*50f);
+            jumping = true;
+        }else if(Input.GetKeyUp(KeyCode.Space)){
+            jumping=false;
         }
+    }
+    // Update is called once per frame
+    Vector3 clipVec(Vector3 tempVec){
+        float newX = tempVec.x;
+        float newY = tempVec.y;
+        float newZ = tempVec.z;
+        if (newX> 4f){
+            newX = 4f;
+        }
+        if (newY> 4f){
+            newY = 4f;
+        }
+        if (newZ> 4f){
+            newZ = 4f;
+        }
+        return new Vector3(newX,newY,newZ);
+    }
+    void Update()
+    {
+        getKeyPresses();
+        transform.eulerAngles =new Vector3(0, transform.eulerAngles.y,0);
+        Debug.Log(transform.eulerAngles);
         if (turning ==1){
             transform.Rotate(-Vector3.up*0.2f);
         }
         if (turning ==2){
             transform.Rotate(Vector3.up*0.2f);
+        }
+        if (going_forward){
+            myRigidbody.velocity = clipVec(myRigidbody.velocity+transform.forward);
+        }
+        if(going_right){
+            myRigidbody.velocity = clipVec(myRigidbody.velocity+transform.right);
+
+        }
+        if(going_left){
+            myRigidbody.velocity = clipVec(myRigidbody.velocity-transform.right);
+
+        }
+        if(going_back){
+            myRigidbody.velocity = clipVec(myRigidbody.velocity-transform.forward);
+
+        }
+        if (jumping){
+            myRigidbody.velocity = clipVec(myRigidbody.velocity+Vector3.up);
+
         }
     }
 }
